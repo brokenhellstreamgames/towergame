885433183c841b8dc325fa6bab889fc8d9ebaeee
Benjamin <bousquieres.benjamin@gmail.com>
2021-08-24 16:28:03 +0200

    Remove some folders / Fix RTS camera prefab / Move folders

--- Assets/Script/Shop/Data/ItemData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemData", menuName = "Infinite/Shop/ItemData")]
-public class ItemData : ScriptableObject
-{
-    [SerializeField] private Texture2D icon;
-}
