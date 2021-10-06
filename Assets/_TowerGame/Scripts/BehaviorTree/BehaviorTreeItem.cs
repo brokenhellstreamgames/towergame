66ac810db7ade08dcd41706b34f1706aa32a7281
Benjamin <bousquieres.benjamin@gmail.com>
2021-10-06 21:40:36 +0200

    Remove All

---
+++ Assets/_TowerGame/Scripts/BehaviorTree/BehaviorTreeItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorTreeItem
+{
+    public Condition condition;
+    public Hability hability;
+    public Maxim maxim;
+}
--- Assets/_TowerGame/Scripts/_old/Battle/Data/CharacterData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CharacterData", menuName = "Infinite/Battle/CharacterData")]
-public class CharacterData : UnitData
-{
-    //public bool 
-}
